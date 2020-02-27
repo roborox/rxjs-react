e089be190c7af7fbeb98ca0a9ad465774bdd035a
Roborox dev <dev@roborox.org>
2020-02-27 21:55:08 +0300

    chore(*): more tests

--- src/rx-bool.spec.tsx
+++ src/rx-bool.spec.tsx
@@ -4,8 +4,26 @@ import {Atom} from "@grammarly/focal/dist/_cjs/src/atom"
 import {RxBool} from "./rx-bool"
 import {act} from "react-dom/test-utils"
 
+let counter = 0
+
+function Count() {
+	counter = counter + 1
+	return <span data-testid="value">text</span>
+}
+
 describe("RxBool", () => {
-	test("should render children if true", async() => {
+	test("children are not rendered if not true", async () => {
+		expect.assertions(4)
+		const bool = Atom.create<boolean>(false)
+		const r = render(<RxBool value={bool}><Count/></RxBool>)
+		await expect(r.findByTestId("value")).rejects.toBeTruthy()
+		expect(counter).toBe(0)
+		act(() => bool.set(true))
+		await expect(r.findByTestId("value")).resolves.toBeTruthy()
+		expect(counter).toBe(1)
+	})
+
+	test("should render children if true", async () => {
 		expect.assertions(2)
 		const bool = Atom.create<boolean>(true)
 		const r = render(<RxBool value={bool}><span data-testid="value">test string</span></RxBool>)
@@ -14,7 +32,7 @@ describe("RxBool", () => {
 		await expect(r.findByTestId("value")).rejects.toBeTruthy()
 	})
 
-	test("should work with negate", async() => {
+	test("should work with negate", async () => {
 		expect.assertions(2)
 		const bool = Atom.create<boolean>(false)
 		const r = render(<RxBool value={bool} negate><span data-testid="value">test string</span></RxBool>)
