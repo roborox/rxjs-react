33d07a9b1ebec17f92c2999ddee3ed019ad907bc
vwpo <vwpo@roborox.org>
2020-03-16 14:38:29 +0300

    test(*): add loader test with rx inside

--- src/loader.spec.tsx
+++ src/loader.spec.tsx
@@ -4,6 +4,7 @@ import { Loader } from "./loader"
 import { toRx } from "./to-rx"
 import { act } from "react-dom/test-utils"
 import { F } from "@grammarly/focal"
+import { Rx } from "./rx"
 
 describe("Loader", () => {
 	test("should display loading if is loading", async () => {
@@ -31,12 +32,36 @@ describe("Loader", () => {
 				</Loader>
 			</span>,
 		)
-		await expect(r.getByTestId("test")).toHaveTextContent("loading")
+		expect(r.getByTestId("test")).toHaveTextContent("loading")
 		const number = Math.random()
 		await act(async () => {
 			resolve(number)
 			await promise
 		})
-		await expect(r.getByTestId("test")).toHaveTextContent(number.toString())
+		expect(r.getByTestId("test")).toHaveTextContent(number.toString())
+	})
+
+	test("should work with <Rx /> component", async () => {
+		expect.assertions(3)
+		let resolve: (value: number) => void
+		const promise = new Promise<number>((res) => resolve = res)
+		const [value, status] = toRx(promise)
+		const r = render(
+			<span data-testid="test">
+				<Loader status={status} loading={<span>loading</span>}>
+					<Rx value={value}>
+						{renderable => <span data-testid="content">{renderable}</span>}
+					</Rx>
+				</Loader>
+			</span>,
+		)
+		expect(r.getByTestId("test")).toHaveTextContent("loading")
+		const number = Math.random()
+		await act(async () => {
+			resolve(number)
+			await promise
+		})
+		expect(r.getByTestId("content")).toBeTruthy()
+		expect(r.getByTestId("content")).toHaveTextContent(number.toString())
 	})
 })
