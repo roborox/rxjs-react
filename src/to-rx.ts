a38fdd099da393f0b351b7839f115c203c01dc9a
vwpo <vwpo@roborox.org>
2020-03-17 16:33:31 +0300

    fix(*): fix import issue

--- src/to-rx.ts
+++ src/to-rx.ts
@@ -1,11 +1,12 @@
 import { Observable, ReplaySubject } from "rxjs"
-import { loadingStatusLoading, loadingStatusSuccess } from "."
-import { LoadingStatus, createLoadingStatusError } from "./loading-state"
+import { LoadingStatus, createLoadingStatusError, loadingStatusLoading, loadingStatusSuccess } from "./loading-state"
 
 export function toRx<T>(promise: Promise<T>): [Observable<T>, Observable<LoadingStatus>] {
 	const result = new ReplaySubject<T>(1)
 	const status = new ReplaySubject<LoadingStatus>(1)
+
 	status.next(loadingStatusLoading)
+
 	promise
 		.then(x => {
 			result.next(x)
