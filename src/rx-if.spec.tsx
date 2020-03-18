f51f6b818681e9510fbc0663f58f5df63c3bd243
Roborox dev <dev@roborox.org>
2020-03-18 07:11:09 +0300

    fix(*): improve test performance

--- src/rx-if.spec.tsx
+++ src/rx-if.spec.tsx
@@ -17,32 +17,32 @@ function Test({value}: {value: string}) {
 
 describe("RxIf", () => {
 	test("children are not rendered if test is not true", async () => {
-		expect.assertions(4)
+		expect.assertions(3)
 		const bool = Atom.create<boolean>(false)
 		const r = render(<RxIf test={bool}><Count/></RxIf>)
-		await expect(r.findByTestId("value")).rejects.toBeTruthy()
+		expect(() => r.getByTestId("value")).toThrow()
 		expect(counter).toBe(0)
 		act(() => bool.set(true))
-		await expect(r.findByTestId("value")).resolves.toBeTruthy()
+		r.getByTestId("value")
 		expect(counter).toBe(1)
 	})
 
 	test("should render children if true", async () => {
-		expect.assertions(2)
+		expect.assertions(1)
 		const bool = Atom.create<boolean>(true)
 		const r = render(<RxIf test={bool}><span data-testid="value">test string</span></RxIf>)
-		await expect(r.findByTestId("value")).resolves.toBeTruthy()
+		r.getByTestId("value")
 		act(() => bool.set(false))
-		await expect(r.findByTestId("value")).rejects.toBeTruthy()
+		expect(() => r.getByTestId("value")).toThrow()
 	})
 
 	test("should work with negate", async () => {
-		expect.assertions(2)
+		expect.assertions(1)
 		const bool = Atom.create<boolean>(false)
 		const r = render(<RxIf test={bool} negate><span data-testid="value">test string</span></RxIf>)
-		await expect(r.findByTestId("value")).resolves.toBeTruthy()
+		r.getByTestId("value")
 		act(() => bool.set(true))
-		await expect(r.findByTestId("value")).rejects.toBeTruthy()
+		expect(() => r.getByTestId("value")).toThrow()
 	})
 
 	test("should render else part if not true", () => {
